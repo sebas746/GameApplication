f7e56321b09179c2ff23a2a77af46943e7ba8d92
sebas746 <sebas746@hotmail.com>
2019-10-06 18:26:09 -0500

    Added unit test cases.

---
+++ GamingApp.Dac.Test/GamingApp.DAC.Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using GamingApp.DAC.DataAccess;
+using GamingApp.Domain.DataContext.GamingApp;
+using GamingApp.Domain.Interfaces.DAC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using GamingApp.DAC.DataAccess.GamingApp;
+using GamingApp.Domain.Entities.DTO;
+
+namespace GamingApp.Dac.Test
+{
+    [TestClass]
+    public class GamingAppTest
+    {
+        private IUnitOfWork _UnitofWork;
+        private IGamingAppDAC _GamingAppDAC;
+        private GamingAppDataContext dbContext; 
+
+        [TestInitialize]
+        public void Init()
+        {
+            dbContext = new GamingAppDataContext();
+            _UnitofWork = new UnitOfWork(dbContext);
+            _GamingAppDAC = new GamingAppDAC();
+        }
+        
+
+        [TestMethod]
+        public void GetStatisticsTest()
+        {
+            //Arrange
+            IEnumerable<GameStatistics> response; 
+
+            //Act
+            response = _UnitofWork.GameStatistics.Get();
+
+
+            //Asserts
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Count() > 0);
+        }
+
+        [TestMethod]
+        public void GetRankingTest()
+        {
+            //Arrange
+            IEnumerable<Ranking> response;
+
+            //Act
+            response = _GamingAppDAC.GetTopPlayers();
+
+
+            //Asserts
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Count() > 0);
+        }
+    }
+}
---
+++ GamingApp.Dac.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GamingApp.Dac.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GamingApp.Dac.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b031f83-d89e-4594-ade6-fdf8ce2cba3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
